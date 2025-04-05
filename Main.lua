4c5163eb593a2cda892329df290a901064c7daa1
ViodTerimer <sorge2v2007@gmail.com>
2025-04-05 22:29:40 +0200

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 4639625707 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ViodTerimer/_xXx_/refs/heads/main/WT.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ViodTerimer/_xXx_/refs/heads/main/Uni.lua))()
+end
