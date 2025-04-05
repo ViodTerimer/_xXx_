e1269fdf6f877b1a95fc51a3084a148357975132
ViodTerimer <sorge2v2007@gmail.com>
2025-04-05 22:34:49 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 4639625707 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ViodTerimer/_xXx_/refs/heads/main/WT.lua"))()
 else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/ViodTerimer/_xXx_/refs/heads/main/Uni.lua))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/ViodTerimer/_xXx_/refs/heads/main/Uni.lua"))()
 end
