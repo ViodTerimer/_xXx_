1e4cee4f255666904da71789206dac668015a6d7
ViodTerimer <sorge2v2007@gmail.com>
2025-04-05 22:33:17 +0200

    Update Uni.lua

--- Uni.lua
+++ Uni.lua
@@ -3,7 +3,7 @@ local function LaunchNH_Uni(LTheme)
 
 local version = "0.9.8"
 local DC = "https://discord.gg/EEMafffGeY"
-local Keys = {"669223", "WTHP"}
+local Key = {"669223", "WTHP"}
 local PW = "WTHP-Vincent"
 local NHIcon = 7733752575
 
@@ -70,7 +70,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "WTHP", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = Keys -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = Key -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
