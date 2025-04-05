758c6384bdb07074b73548cf0e319cade6ad7e4f
ViodTerimer <sorge2v2007@gmail.com>
2025-04-05 22:47:30 +0200

    Update Uni.lua

--- Uni.lua
+++ Uni.lua
@@ -3,7 +3,7 @@ local function LaunchNH_Uni(LTheme)
 
 local version = "0.9.8"
 local DC = "https://discord.gg/EEMafffGeY"
-
+local Key = {"669223", "WTHP"}
 local PW = "WTHP-Vincent"
 local NHIcon = 7733752575
 
