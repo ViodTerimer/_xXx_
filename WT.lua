46d100f85d3af8d46230c424e937c39ead099cad
ViodTerimer <sorge2v2007@gmail.com>
2025-04-05 23:11:39 +0200

    Update WT.lua

--- WT.lua
+++ WT.lua
@@ -1,7 +1,7 @@
 local StartTheme = "AmberGlow"
 local function LaunchNH_WT(LTheme)
 
-local version = "0.9.9"
+local version = "0.9.8.nigger"
 local DC = "https://discord.gg/EEMafffGeY"
 local Key = {"669223", "WTHP"}
 local NHIcon = 7733752575
