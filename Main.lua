13aef4eeaab3b95af62a66e69a12554f3b32088c
ViodTerimer <sorge2v2007@gmail.com>
2025-04-05 22:41:37 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,5 @@
+local Key = {"669223", "WTHP"}
+
 if game.PlaceId == 4639625707 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ViodTerimer/_xXx_/refs/heads/main/WT.lua"))()
 else
